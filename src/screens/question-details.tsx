b29ade4241aa34656b225f5d2a27fc3219cf2ef8
Igor <iigorfelipe@gmail.com>
2024-11-19 21:47:52 -0300

    feat: handle question deletion and add formatTimestampToDate function

--- src/screens/question-details.tsx
+++ src/screens/question-details.tsx
@@ -1,8 +1,12 @@
 import { MaterialIcons } from '@expo/vector-icons';
-import { RouteProp } from '@react-navigation/native';
-import { ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import { NavigationProp, RouteProp } from '@react-navigation/native';
+import { Alert, ScrollView, Text, TouchableOpacity, View } from 'react-native';
 import { NewQuestionButton } from '../components/navigation-button';
 import { colors } from '../styles/colors';
+import { formatTimestampToDate } from '../utils/format-timestamp';
+import { useQuestionStore } from '../store/use-question-store';
+import { useNavigation } from 'expo-router';
+import { DrawerParamList } from '../types/question';
 
 type TouchableOpacityCustomProps = {
   iconName: keyof typeof MaterialIcons.glyphMap;
@@ -47,18 +51,42 @@ function TouchableOpacityCustom2({
 
 type QuestionDetailsRouteProp = RouteProp<
   {
-    QuestionDetail: { id: string; question: string; answer: string; date: string; fav: boolean };
+    QuestionDetail: { id: string; question: string; answer: string; created: number };
   },
   'QuestionDetail'
 >;
 
 export default function QuestionDetails({ route }: { route: QuestionDetailsRouteProp }) {
-  const { question, answer, date, fav } = route.params;
+  const navigation = useNavigation<NavigationProp<DrawerParamList>>();
+  const { id, question, answer, created } = route.params;
+  const { toggleFavorite, questions } = useQuestionStore();
+  const formatedDate = formatTimestampToDate(created);
+
+  const isFavQuestion = questions.find((item) => item.id === id)?.fav;
+
+  const handleDelete = () => {
+    Alert.alert(
+      'Excluir pergunta',
+      'Tem certeza que deseja excluir esta pergunta? Ela será removida dos favoritos também.',
+      [
+        { text: 'Cancelar', style: 'cancel' },
+        {
+          text: 'Excluir',
+          style: 'destructive',
+          onPress: () => {
+            const questionStore = useQuestionStore.getState();
+            questionStore.removeQuestion(id);
+            navigation.navigate('Home');
+          },
+        },
+      ],
+    );
+  };
 
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1, paddingLeft: 16, paddingRight: 16, gap: 8 }}>
       <View className="p-3 w-24 bg-white rounded-3xl mt-2 mx-auto">
-        <Text className="font-semibold text-base">{date}</Text>
+        <Text className="font-semibold text-base">{formatedDate}</Text>
       </View>
 
       <View className="flex flex-col bg-white rounded-3xl p-4 gap-8">
@@ -112,12 +140,12 @@ export default function QuestionDetails({ route }: { route: QuestionDetailsRoute
       <View className="flex flex-row items-center justify-evenly bg-white rounded-3xl py-5 gap-2">
         <TouchableOpacityCustom2 iconName="ios-share" text="Compartilhar" onPress={() => {}} />
         <TouchableOpacityCustom2
-          iconName={`${fav ? 'star' : 'star-outline'}`}
-          text={`${fav ? 'Desfavoritar' : 'Favoritar'}`}
-          onPress={() => {}}
-          iconColor={`${fav ? colors.primary : ''}`}
+          iconName={`${isFavQuestion ? 'star' : 'star-outline'}`}
+          text={`${isFavQuestion ? 'Desfavoritar' : 'Favoritar'}`}
+          onPress={() => toggleFavorite(id)}
+          iconColor={`${isFavQuestion ? colors.primary : ''}`}
         />
-        <TouchableOpacityCustom2 iconName="delete-outline" text="Excluir" onPress={() => {}} />
+        <TouchableOpacityCustom2 iconName="delete-outline" text="Excluir" onPress={handleDelete} />
       </View>
 
       <NewQuestionButton />
