da7cc0092290cd9561489fd045c18ae88724c50e
Igor <iigorfelipe@gmail.com>
2024-11-21 10:45:02 -0300

    feat: implements formatTimestampToRelativeDate function

--- src/utils/format-timestamp.ts
+++ src/utils/format-timestamp.ts
@@ -1,4 +1,26 @@
-import { format } from 'date-fns';
+import { format, differenceInCalendarDays } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 export const formatTimestampToDate = (unixTimestamp: number) => {
   return format(new Date(unixTimestamp * 1000), 'dd/MM/yyyy');
+};
+
+export const formatTimestampToRelativeDate = (timestamp: number): string => {
+  const now = new Date();
+  const date = new Date(timestamp * 1000);
+
+  const diffInDays = differenceInCalendarDays(now, date);
+
+  if (diffInDays === 0) {
+    return 'Hoje';
+  }
+
+  if (diffInDays === 1) {
+    return 'Ontem';
+  }
+
+  if (diffInDays <= 7) {
+    return `${diffInDays} dias atrás`;
+  }
+
+  return format(date, 'dd/MM/yyyy', { locale: ptBR });
 };
