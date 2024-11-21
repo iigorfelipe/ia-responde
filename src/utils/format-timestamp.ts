722e79abb6a7bda763bc98c079cb03dc1c7ba04a
Igor <iigorfelipe@gmail.com>
2024-11-21 10:32:31 -0300

    refactor: update formatTimestampToDate function using date-fns

--- src/utils/format-timestamp.ts
+++ src/utils/format-timestamp.ts
@@ -1,8 +1,4 @@
+import { format } from 'date-fns';
 export const formatTimestampToDate = (unixTimestamp: number) => {
-  const date = new Date(unixTimestamp * 1000);
-  const day = String(date.getDate()).padStart(2, '0');
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const year = date.getFullYear();
-
-  return `${day}/${month}/${year}`;
+  return format(new Date(unixTimestamp * 1000), 'dd/MM/yyyy');
 };
