b953e5c3dece14753afa68e7163e489fd2a7d61a
Igor <iigorfelipe@gmail.com>
2024-11-26 14:39:44 -0300

    deploy: deployment using EAS update

--- app.config.js
+++ app.config.js
@@ -28,6 +28,15 @@ export default {
     },
     extra: {
       OPEN_IA_API_KEY: process.env.OPEN_IA_API_KEY,
+      eas: {
+        projectId: 'd34507d7-c465-4ddf-b9c0-b0b5aad3c7a8',
+      },
+    },
+    updates: {
+      url: 'https://u.expo.dev/d34507d7-c465-4ddf-b9c0-b0b5aad3c7a8',
+    },
+    runtimeVersion: {
+      policy: 'appVersion',
     },
   },
 };
